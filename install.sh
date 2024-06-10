709c26b8293ae2043f449d50f5ed02b281fbf3a4
Kolandone <sjamshid81@gmail.com>
2024-06-10 17:01:52 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
  apt update -y
-apt install wireguard-tools jq xz-utils bzip2 -y
+apt install jq xz-utils bzip2 -y
 curl -o $PREFIX/bin/KOLAND https://raw.githubusercontent.com/Kolandone/Hidify/main/KOLAND.sh
 chmod +x $PREFIX/bin/KOLAND
