85c3b0a71b0a2fe72bfdba3582b3250e22bf4dde
Kolandone <sjamshid81@gmail.com>
2024-08-14 02:01:36 +0330

    Update k.sh

--- k.sh
+++ k.sh
@@ -94,8 +94,7 @@ if [ "$user_choice" == "1" ]; then
 	link6="warp://${license}@${ipv4}?ifp=1-3&ifps=10-20&ifpd=5-10&ifpm=m1#m1&&detour=warp://p2@${ipv4}#WoW"
 	link7="warp://${license}@${ipv4}??ifp=1-3&ifps=10-20&ifpd=5-10&ifpm=h_30#hex&&detour=warp://p2@${ipv4}#WoW"
   fi
-else
-if [ "$user_choice" == "2" ]; then
+elif [ "$user_choice" == "2" ]; then
   # Use IPv6 address for the primary part and IPv4 address for the detour part
   
   if [[ -z "$license" ]]; then
@@ -116,8 +115,7 @@ if [ "$user_choice" == "2" ]; then
 	link7="warp://${license}@${ipv6}?ifp=1-3&ifps=10-20&ifpd=5-10&ifpm=h_30#hex&&detour=warp://p2@${ipv4}#WoW"
    fi
   fi
-else
-  if [ "$user_choice" == "3" ]; then
+  elif [ "$user_choice" == "3" ]; then
   # Use IPv4 address for both parts of the link
   if [[ -z "$license" ]]; then
     link1="warp://p1@${ipv4}ifp=1-3&ifps=10-20&ifpd=5-10&ifpm=m1#m1&&detour=warp://p2@${ipv41}#WoW"
